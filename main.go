7e2fa19a97aa86c85711dd105bc90fe5a0488314
Pranay-Tej <s.pranayteja@gmail.com>
2025-04-24 23:24:31 +0530

    add handler funcs

--- main.go
+++ main.go
@@ -13,11 +13,19 @@ func main() {
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", handleHealthz)
 
 	log.Printf("Serving on port: %s\n", port)
 
 	if err := httpServer.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
